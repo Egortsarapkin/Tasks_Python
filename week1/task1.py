973ebcc2b6c6bc1a47dd9605dde639ee71fef9b5
Tsarapkin Egor <egortsarapkin228@gmail.com>
2024-09-19 11:37:44 +0300

    add task1

---
+++ week1/task1.py
@@ -0,0 +1,25 @@
+import math
+
+
+class Solution(object):
+
+    def longestPalindrome(self, s):
+        def expanda(st, left, right):
+            while (left >= 0 and right < len(st) and st[left] == st[right]):
+                left -= 1
+                right += 1
+
+            return right - left - 1
+
+        end = 0
+        start = 0
+        for i in range(0, len(s)):
+            len1 = expanda(s, i, i)
+            len2 = expanda(s, i, i + 1)
+            length = max(len1, len2)
+            if length > end - start:
+                start = math.ceil(i - (length - 1) / 2)
+                end = math.floor(i + length / 2)
+
+        return (s[int(start):(int(end + 1))])
+
